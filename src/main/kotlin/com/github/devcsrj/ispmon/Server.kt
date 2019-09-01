2be83d26bc77a12106b26d17dacbbe3726218510
Reijhanniel Jearl Campos <devcsrj@goeden.ph>
2019-09-01 20:09:08 +0800

    ✨ Schedule speedtest 30 mins default

--- src/main/kotlin/com/github/devcsrj/ispmon/Server.kt
+++ src/main/kotlin/com/github/devcsrj/ispmon/Server.kt
@@ -2,24 +2,34 @@ package com.github.devcsrj.ispmon
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.github.devcsrj.ookla.Speedtest
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Context
 import io.vertx.core.Future
 import io.vertx.core.Launcher
 import io.vertx.core.Vertx
 import io.vertx.core.http.HttpMethod
+import io.vertx.core.logging.LoggerFactory
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.RoutingContext
 import io.vertx.ext.web.handler.StaticHandler
 import java.nio.file.Files
 import java.nio.file.Paths
+import java.time.Duration
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+import java.util.concurrent.Executors
+import java.util.concurrent.ScheduledExecutorService
+import java.util.concurrent.TimeUnit
 
 class Server : AbstractVerticle() {
 
+  private val logger = LoggerFactory.getLogger(Server::class.java)
+  private val timeout = Duration.ofSeconds(15)
+
   private lateinit var repo: ResultRepository
   private lateinit var mapper: ObjectMapper
+  private lateinit var scheduler: ScheduledExecutorService
 
   override fun init(vertx: Vertx,
                     context: Context) {
@@ -31,6 +41,16 @@ class Server : AbstractVerticle() {
 
     mapper = ObjectMapper()
     mapper.registerModule(KotlinModule())
+
+    val monitorInterval = config().getLong("MONITOR_INTERVAL", 30L)
+    scheduler = Executors.newSingleThreadScheduledExecutor()
+    scheduler.scheduleWithFixedDelay(
+      speedtest(), 0L, monitorInterval, TimeUnit.MINUTES
+    )
+  }
+
+  override fun stop() {
+    scheduler.shutdown()
   }
 
   override fun start(future: Future<Void>) {
@@ -71,6 +91,18 @@ class Server : AbstractVerticle() {
     context
       .response()
       .end(mapper.writeValueAsString(results))
+  }
+
+  private fun speedtest(): Runnable {
+    return Runnable {
+      val test = Speedtest(timeout)
+      try {
+        val result = test.call()
+        repo.save(result)
+      } catch (e: Throwable) {
+        logger.error("Could not finish speedtest due to exception", e)
+      }
+    }
   }
 }
 
