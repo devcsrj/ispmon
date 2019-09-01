d12c1911b6c53162ee6e204169e564db9b326ee2
Reijhanniel Jearl Campos <devcsrj@goeden.ph>
2019-09-01 09:50:27 +0800

    ✨ Implement http handlers

--- src/main/kotlin/com/github/devcsrj/ispmon/Server.kt
+++ src/main/kotlin/com/github/devcsrj/ispmon/Server.kt
@@ -1,16 +1,69 @@
 package com.github.devcsrj.ispmon
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import io.vertx.core.AbstractVerticle
+import io.vertx.core.Context
+import io.vertx.core.Future
 import io.vertx.core.Launcher
+import io.vertx.core.Vertx
+import io.vertx.core.http.HttpMethod
+import io.vertx.ext.web.Router
+import io.vertx.ext.web.RoutingContext
+import io.vertx.ext.web.handler.StaticHandler
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class Server : AbstractVerticle() {
 
-  override fun start() {
+  private lateinit var repo: ResultRepository
+  private lateinit var mapper: ObjectMapper
+
+  override fun init(vertx: Vertx,
+                    context: Context) {
+    super.init(vertx, context)
+
+    val resultsDir = Paths.get("results")
+    Files.createDirectories(resultsDir)
+    repo = DiskResultRepository(resultsDir)
+
+    mapper = ObjectMapper()
+    mapper.registerModule(KotlinModule())
+  }
+
+  override fun start(future: Future<Void>) {
+    val router = Router.router(vertx).apply {
+      route(HttpMethod.GET, "/static/*")
+        .handler(StaticHandler.create("static"))
+      route(HttpMethod.GET, "/results")
+        .produces("application/json")
+        .handler { getResults(it) }
+      route(HttpMethod.GET, "/")
+        .produces("text/html")
+        .handler { it.response().sendFile("static/index.html") }
+    }
+
     vertx.createHttpServer()
-      .requestHandler { req -> req.response().end("Hello Vert.x!") }
+      .requestHandler(router)
       .listen(8080)
   }
 
+  private fun getResults(context: RoutingContext) {
+    val since = context.queryParam("since").let {
+      if (it.isEmpty())
+        LocalDate.now()
+      else {
+        val pattern = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        LocalDate.parse(it.iterator().next(), pattern)
+      }
+    }
+    val results = repo.findSince(since)
+    context
+      .response()
+      .end(mapper.writeValueAsString(results))
+  }
 }
 
 fun main() {
